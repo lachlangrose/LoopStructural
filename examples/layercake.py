437ee8df53dcaa699f75fd2b161db9e620de613e
lachlangrose <lachlan.grose@monash.edu>
2026-04-22 12:22:23 +0930

    docs: adding layercake data

--- examples/layercake.py
+++ examples/layercake.py
@@ -1,17 +1,19 @@
 import numpy as np
 from loop_common.observations import PointSet
+from loop_model.features import GeologicalUnit
+from loop_model.manager import GeologicalSchema
 
 # Mock data for 4 layers (Basal contacts)
 # Layer 0 is the bottom-most basal contact
+xy = np.random.rand(10, 2)  # 10 random points in XY
+
 contact_data = [
-    PointSet(name="Base_L1", coords=np.random.rand(10, 3)),
-    PointSet(name="Base_L2", coords=np.random.rand(10, 3)),
-    PointSet(name="Base_L3", coords=np.random.rand(10, 3)),
-    PointSet(name="Base_L4", coords=np.random.rand(10, 3)),
+    PointSet(name="Base_L1", coords=np.hstack([xy, np.zeros((10, 1))])),
+    PointSet(name="Base_L2", coords=np.hstack([xy, np.zeros((10, 1)) + 1.0])),
+    PointSet(name="Base_L3", coords=np.hstack([xy, np.zeros((10, 1)) + 2.0])),
+    PointSet(name="Base_L4", coords=np.hstack([xy, np.zeros((10, 1)) + 3.0])),
 ]
 
-from loop_model.features import GeologicalUnit
-from loop_model.manager import GeologicalSchema
 
 schema = GeologicalSchema(name="Simple 4-Layer Model")
 
@@ -33,4 +35,4 @@ for i, name in enumerate(unit_names):
 for i in range(len(units) - 1):
     schema.add_relation(master_id=units[i + 1].uuid, slave_id=units[i].uuid, relation="overlies")
 
-schema.visualize_graph(include_data=True)
+# schema.visualize_graph(include_data=True)
